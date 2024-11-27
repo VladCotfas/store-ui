4dacaf7a96601e27bde1d6512e606fa2075db60b
VladCotfas <vladutcotfas@gmail.com>
2024-11-27 15:28:43 +0200

    Folder structure

---
+++ src/app/features/features.routing.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { FeaturesComponent } from "./features.component";
+
+export const FeatureRoutes: Routes = [
+    {
+        path: '',
+        component: FeaturesComponent
+    }
+];
